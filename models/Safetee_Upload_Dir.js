83815e8c6d768a05fb72d4c156f8e62923c2c493
age2ho <age2ho@gmail.com>
2016-11-03 11:21:41 +0100

    initials 1

--- models/Safetee_Upload_Dir.js
+++ models/Safetee_Upload_Dir.js
@@ -7,7 +7,7 @@ var Audio_Upload_Dir = ({
     //
     audio_dir_heroku: function (req) {
         //console.log(__dirname);
-        return '/../../app/public/records';
+        return '/app/public/records';
     }
     //
 });
