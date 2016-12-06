cbb8f4cf3adf33cfda4409cd20b27b7d684724c0
age2ho <age2ho@gmail.com>
2016-12-06 13:28:43 +0100

    admin fixing for reports

--- models/Menu.js
+++ models/Menu.js
@@ -3,6 +3,7 @@ exports.admin = function () {
     //
     return '<li><a href="./home"><i class="zmdi zmdi-home"></i> Home</a></li>' +
            '<li><a href="./records"><i class="zmdi zmdi-chart"></i> Records</a></li>' +
+           '<li><a href="./reports"><i class="fa fa-list-alt"></i> Reports</a></li>' +
            '<li><a href="./users"><i class="fa fa-group"></i> Users</a></li>' +
            '<li><a href="./agencies"><i class="fa fa-institution"></i> Agencies</a></li>' +
            '<li><a href="./tips"><i class="fa fa-asterisk"></i> Tips</a></li>' +
